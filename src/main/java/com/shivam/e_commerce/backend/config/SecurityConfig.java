bef261c475251ab2bf342fa507d9d394918168b8
hii <shivamteotia853@gmail.com>
2026-06-11 12:49:40 +0530

    Project Completed

--- src/main/java/com/shivam/e_commerce/backend/config/SecurityConfig.java
+++ src/main/java/com/shivam/e_commerce/backend/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
